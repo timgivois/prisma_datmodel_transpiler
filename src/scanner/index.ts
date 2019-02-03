597a5bb7e4f5445c5002e511bf393159d276e94d
Tim Givois <tim.givois.mendez@gmail.com>
2019-02-03 03:14:44 -0600

    refactor preprocess

--- src/scanner/index.ts
+++ src/scanner/index.ts
@@ -1,28 +1,42 @@
 import tokens, { variableRegex, variableToken, Token, Tokens } from './tokens';
 import { ParseError } from '../errors'
 
-const preprocess = (dataModel: string) => {
-  // TODO: Refactor this shit haha
-  const extraEnters = new RegExp(/\n\n+/, 'g')
-  const extraSpaces = new RegExp(/  +/, 'gm')
-  const trimSpaces = new RegExp(/^ +| +$/, 'gm')
-  const equality = new RegExp(/:/, 'g')
-  const openBraces = new RegExp(/\[/, 'g')
-  const closeBraces = new RegExp(/\]/, 'g')
-  const exclamation = new RegExp(/!/, 'g')
-
-  const withoutExtraEnters = dataModel.replace(extraEnters, '\n')
-  const withEqualitySpaces = withoutExtraEnters.replace(equality, ' : ')
-  const withBracesSpaces = withEqualitySpaces.replace(openBraces, ' [ ').replace(closeBraces, ' ] ')
-  const withExclammationSpaces = withBracesSpaces.replace(exclamation, ' ! ')
-  const withoutExtraSpaces = withExclammationSpaces.replace(extraSpaces, ' ')
-  const withTrimedSpaces = withoutExtraSpaces.replace(trimSpaces, '')
-
-
-  return withTrimedSpaces
+interface replacable {
+  regex: RegExp,
+  by: string
 }
 
+const replaceMap:Array<replacable> = [
+  {
+    regex: new RegExp(/\n\n+/, 'g'),
+    by: '\n'
+  },
+  {
+    regex: new RegExp(/:/, 'g'),
+    by: ' : '
+  },{
+    regex: new RegExp(/\[/, 'g'),
+    by: ' [ '
+  }, {
+    regex: new RegExp(/\]/, 'g'),
+    by: ' ] '
+  }, {
+    regex: new RegExp(/!/, 'g'),
+    by: ' ! '
+  },{
+    regex: new RegExp(/  +/, 'gm'),
+    by: ' '
+  },{
+    regex: new RegExp(/^ +| +$/, 'gm'),
+    by: ''
+  }
+]
 
+const preprocess = (dataModel: string) => {
+  return replaceMap.reduce((procesedDM:string, pipeline:replacable):string => {
+    return procesedDM.replace(pipeline.regex, pipeline.by)
+  }, dataModel)
+}
 
 const scan = (dataModel: string) => {
   const delimiter = new RegExp(/\s+/, 'g')
